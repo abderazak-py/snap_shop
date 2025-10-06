e455735d926f6d2249adf3fe199cdf2662e574a6
abderazak <achourabderazak1@gmail.com>
2025-10-06 13:48:54 +0100

    first commit snapshop

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const SnapShop());
+}
+
+class SnapShop extends StatelessWidget {
+  const SnapShop({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
