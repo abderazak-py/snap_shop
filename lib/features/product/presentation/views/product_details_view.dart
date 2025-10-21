ef3ce3c24bd5040648871516b4391ef270723a40
Abderazak Achour <71965021+abderazak-py@users.noreply.github.com>
2025-10-21 17:41:18 +0100

    added product details

--- lib/features/product/presentation/views/product_details_view.dart
+++ lib/features/product/presentation/views/product_details_view.dart
@@ -1,5 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:snap_shop/core/utils/app_router.dart';
+import 'package:snap_shop/core/utils/constants.dart';
+import 'package:snap_shop/core/utils/styles.dart';
+import 'package:snap_shop/features/auth/presentation/views/widgets/custom_big_button.dart';
 import 'package:snap_shop/features/product/domain/entities/product_entity.dart';
 
 class ProductDetailsView extends StatelessWidget {
@@ -8,41 +15,159 @@ class ProductDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final controller = PageController();
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('name: ${product.name}'),
-            Text('description: ${product.description}'),
-            Text('price: ${product.price}'),
-            Text('category: ${product.category}'),
-            Text('createdAt: ${product.createdAt}'),
-            CachedNetworkImage(imageUrl: product.images.first ?? ''),
+            SizedBox(height: height * 0.07),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                GestureDetector(
+                  onTap: () => GoRouter.of(context).pop(),
+                  child: Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: AppColors.kTextColor,
+                    size: 30,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  'Product Details',
+                  style: Styles.titleText20.copyWith(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () => GoRouter.of(context).push(AppRouter.kCartView),
+                  child: SvgPicture.asset(
+                    width: 30,
+                    AppIcons.cart,
+                    colorFilter: ColorFilter.mode(
+                      AppColors.kTextColor,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 12),
+              ],
+            ),
+            SizedBox(height: height * 0.04),
+            SizedBox(
+              width: width,
+              height:
+                  width -
+                  60, //if u clone this u can use a variable p for padding and use 2p here
+              child: PageView(
+                controller: controller,
+                children: [
+                  for (var image in product.images)
+                    if (image != null)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: ClipRRect(
+                          borderRadius: BorderRadiusGeometry.circular(16),
+                          child: CachedNetworkImage(
+                            imageUrl: image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Center(
+              child: SmoothPageIndicator(
+                controller: controller,
+                count: product.images.length,
+                effect: WormEffect(
+                  dotHeight: 8,
+                  dotWidth: 8,
+                  activeDotColor: AppColors.kPrimaryColor,
+                  dotColor: AppColors.kTextColor2,
+                ),
+              ),
+            ),
+            SizedBox(height: height * 0.04),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                product.name,
+                style: Styles.titleText22.copyWith(fontWeight: FontWeight.w800),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                product.category,
+                style: Styles.titleText16.copyWith(
+                  color: AppColors.kTextColor2,
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                'Description',
+                style: Styles.titleText18.copyWith(fontWeight: FontWeight.w800),
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15),
+              child: Text(
+                product.description,
+                maxLines: 6,
+                overflow: TextOverflow.ellipsis,
+                style: Styles.titleText16.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.kTextColor2,
+                ),
+              ),
+            ),
+            Spacer(),
+            Row(
+              children: [
+                Spacer(),
+                Text(
+                  '\$',
+                  style: Styles.titleText12.copyWith(
+                    fontSize: 34,
+                    fontWeight: FontWeight.w900,
+                    color: AppColors.kPrimaryColor,
+                  ),
+                ),
+                Text(
+                  '${product.price}',
+                  style: Styles.titleText12.copyWith(
+                    fontSize: 34,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                Spacer(),
+                SizedBox(
+                  width: width * 0.48,
+                  child: CustomBigButton(
+                    title: 'Add to Cart',
+                    onPressed: () {
+                      //TODO add functionality
+                    },
+                  ),
+                ),
+                SizedBox(width: width * 0.05),
+              ],
+            ),
+            SizedBox(height: height * 0.05),
           ],
         ),
       ),
     );
   }
 }
-//TODO add this inside product detailes view ,and make this section for adding favorite
-
-                            // try {
-                            //   addOneToCartUsecase.execute(product.id);
-                            //   context.read<CartCubit>().getCartItems();
-                            //   ScaffoldMessenger.of(context).showSnackBar(
-                            //     SnackBar(
-                            //       content: const Text('Product added to cart'),
-                            //       behavior: SnackBarBehavior.floating,
-                            //       margin: const EdgeInsets.all(12),
-                            //       backgroundColor: AppColors.kPrimaryColor,
-                            //       shape: RoundedRectangleBorder(
-                            //         borderRadius: BorderRadius.circular(10),
-                            //       ),
-                            //       duration: const Duration(seconds: 2),
-                            //     ),
-                            //   );
-                            // } catch (e) {
-                            //   debugPrint(e.toString());
-                            // }
--- lib/features/product/presentation/views/widgets/product_card.dart
+++ lib/features/product/presentation/views/widgets/product_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:skeletonizer/skeletonizer.dart';
+import 'package:snap_shop/core/utils/app_router.dart';
 import 'package:snap_shop/core/utils/constants.dart';
 import 'package:snap_shop/core/utils/styles.dart';
 import 'package:snap_shop/features/favorite/presentation/cubit/favorite_cubit.dart';
@@ -18,100 +19,105 @@ class ProductCard extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     final isLoading = Skeletonizer.maybeOf(context)?.enabled ?? false;
 
-    return Column(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadiusGeometry.circular(16),
-          child: isLoading
-              ? Skeleton.replace(
-                  width: width * 0.43,
-                  height: width * 0.43,
-                  child: SizedBox.expand(),
-                )
-              : Stack(
-                  children: [
-                    CachedNetworkImage(
-                      width: width * 0.43,
-                      height: width * 0.43,
-                      fit: BoxFit.cover,
-                      imageUrl: product.images.isNotEmpty
-                          ? product.images.first ?? ''
-                          : '',
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    ),
-                    Positioned(
-                      top: 8,
-                      right: 8,
-                      child: Material(
-                        color: AppColors.kTextColor.withAlpha(90),
-                        shape: const CircleBorder(),
-                        child: InkWell(
-                          customBorder: const CircleBorder(),
-                          onTap: () async {
-                            context.read<FavoriteCubit>().toggleFavorite(
-                              product,
-                            );
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.all(6),
-                            child: BlocBuilder<FavoriteCubit, FavoriteState>(
-                              buildWhen: (prev, curr) {
-                                bool has(FavoriteState s) =>
-                                    s is FavoriteSuccess &&
-                                    s.favorite.any(
-                                      (e) => e.productId == product.id,
-                                    );
-                                return has(prev) != has(curr);
-                              },
-                              builder: (context, state) {
-                                final isFavorite =
-                                    state is FavoriteSuccess &&
-                                    state.favorite.any(
-                                      (e) => e.productId == product.id,
-                                    );
-                                return isFavorite
-                                    ? SvgPicture.asset(
-                                        AppIcons.favoriteFilled,
-                                        width: 18,
-                                        colorFilter: ColorFilter.mode(
-                                          Colors.red,
-                                          BlendMode.srcIn,
-                                        ),
-                                      )
-                                    : SvgPicture.asset(
-                                        AppIcons.favorite,
-                                        width: 18,
-                                        colorFilter: ColorFilter.mode(
-                                          Colors.white,
-                                          BlendMode.srcIn,
-                                        ),
+    return GestureDetector(
+      onTap: () {
+        AppRouter.router.push(AppRouter.kProductDetailsView, extra: product);
+      },
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(16),
+            child: isLoading
+                ? Skeleton.replace(
+                    width: width * 0.43,
+                    height: width * 0.43,
+                    child: SizedBox.expand(),
+                  )
+                : Stack(
+                    children: [
+                      CachedNetworkImage(
+                        width: width * 0.43,
+                        height: width * 0.43,
+                        fit: BoxFit.cover,
+                        imageUrl: product.images.isNotEmpty
+                            ? product.images.first ?? ''
+                            : '',
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      ),
+                      Positioned(
+                        top: 8,
+                        right: 8,
+                        child: Material(
+                          color: AppColors.kTextColor.withAlpha(90),
+                          shape: const CircleBorder(),
+                          child: InkWell(
+                            customBorder: const CircleBorder(),
+                            onTap: () async {
+                              context.read<FavoriteCubit>().toggleFavorite(
+                                product,
+                              );
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.all(6),
+                              child: BlocBuilder<FavoriteCubit, FavoriteState>(
+                                buildWhen: (prev, curr) {
+                                  bool has(FavoriteState s) =>
+                                      s is FavoriteSuccess &&
+                                      s.favorite.any(
+                                        (e) => e.productId == product.id,
                                       );
-                              },
+                                  return has(prev) != has(curr);
+                                },
+                                builder: (context, state) {
+                                  final isFavorite =
+                                      state is FavoriteSuccess &&
+                                      state.favorite.any(
+                                        (e) => e.productId == product.id,
+                                      );
+                                  return isFavorite
+                                      ? SvgPicture.asset(
+                                          AppIcons.favoriteFilled,
+                                          width: 18,
+                                          colorFilter: ColorFilter.mode(
+                                            Colors.red,
+                                            BlendMode.srcIn,
+                                          ),
+                                        )
+                                      : SvgPicture.asset(
+                                          AppIcons.favorite,
+                                          width: 18,
+                                          colorFilter: ColorFilter.mode(
+                                            Colors.white,
+                                            BlendMode.srcIn,
+                                          ),
+                                        );
+                                },
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-        ),
-        SizedBox(height: 10),
-        SizedBox(
-          width: width * 0.43,
-          child: Text(
-            textAlign: TextAlign.center,
-            product.name,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: Styles.titleText14.copyWith(fontWeight: FontWeight.w900),
+                    ],
+                  ),
           ),
-        ),
-        Text(
-          '\$${product.price.toStringAsFixed(2)}',
-          style: Styles.titleText16,
-        ),
-      ],
+          SizedBox(height: 10),
+          SizedBox(
+            width: width * 0.43,
+            child: Text(
+              textAlign: TextAlign.center,
+              product.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Styles.titleText14.copyWith(fontWeight: FontWeight.w900),
+            ),
+          ),
+          Text(
+            '\$${product.price.toStringAsFixed(2)}',
+            style: Styles.titleText16,
+          ),
+        ],
+      ),
     );
   }
 }
